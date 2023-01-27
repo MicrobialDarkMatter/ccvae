01c7b0c896f86d48070bcf89071f5d4569c3e781
Andre Lamurias <andrel@cs.aau.dk>
2023-01-27 11:45:40 +0100

    Restore file contents that were previously in LFS

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-import os
-from . import contigsdataset
-from . import evaluate
-from . import version
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc058d12023312b39558d695d4186f18f82ca0e7c169511b5db9546f8c89ed6d
+size 84
